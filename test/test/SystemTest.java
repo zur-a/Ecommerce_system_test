5509435ce578ca81fa0484b6e544d798eb3bda51
Jurandi <jur.and.jr42@gmail.com>
2022-07-15 17:41:20 -0300

    Definição do teste deveCadastrarUsuario

--- test/test/SystemTest.java
+++ test/test/SystemTest.java
@@ -30,6 +30,29 @@ public class SystemTest {
 		submit.click();
 	}
 	
+	@Test
+	public void deveCadastrarUsuario() {		
+		driver.get("http://www.ecommerce.com/admin/users/create");
+		
+		WebElement nome = driver.findElement(By.name("desperson"));
+		WebElement login = driver.findElement(By.name("deslogin"));
+		WebElement phone = driver.findElement(By.name("nrphone"));
+		WebElement email = driver.findElement(By.name("desemail"));
+		WebElement password = driver.findElement(By.name("despassword"));
+		
+		nome.sendKeys("Gaius Iulius Caesar Octavianus Augustus");
+		login.sendKeys("Augustus");
+		phone.sendKeys("997788665");
+		email.sendKeys("princepsCivitatis@roma.com");
+		password.sendKeys("Roma");
+		
+		WebElement submit = driver.findElement(By.className("btn-success"));
+		
+		submit.click();
+		
+		assertTrue(driver.getPageSource().contains("Iulius"));
+	}
+	
 	@After
 	public void finaliza() {
 		driver.close();
