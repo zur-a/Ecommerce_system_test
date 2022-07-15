bbdda687a57c615bebcf40c8db2e14127587b0d4
Jurandi <jur.and.jr42@gmail.com>
2022-07-15 17:35:10 -0300

    Definição do método Inicializa, responsável por realizar o login na página, antes dos testes

--- test/test/SystemTest.java
+++ test/test/SystemTest.java
@@ -2,13 +2,31 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SystemTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private WebDriver driver;
+	
+	@Before
+	public void inicializa() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hiiii\\Downloads\\driver\\chromedriver.exe");
+		
+		driver.get("http://www.ecommerce.com/admin/login");
+		
+		WebElement user = driver.findElement(By.name("login"));
+		WebElement pass = driver.findElement(By.name("password"));
+				
+		user.sendKeys("admin");
+		pass.sendKeys("admin");
+		
+		WebElement submit = driver.findElement(By.tagName("button"));
+		
+		submit.click();
 	}
 
 }
