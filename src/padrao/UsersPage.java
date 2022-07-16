bbaa60bd1e5fbfdfff1974b0e37e07620cc5931b
Jurandi <jur.and.jr42@gmail.com>
2022-07-16 08:59:45 -0300

    Refatoração do teste deveAlterarUsuarioCadastrado

--- src/padrao/UsersPage.java
+++ src/padrao/UsersPage.java
@@ -23,6 +23,36 @@ public class UsersPage {
 		driver.switchTo().alert().accept();
 	}
 	
+	public void alterar() {
+		WebElement alter = driver.findElement(By.xpath("/html/body/div/div[1]/section[2]/div/div/div/div[2]/table/tbody/tr[2]/td[6]/a[1]"));
+		alter.click();
+	}
+	
+	public void preencherForm(String name, String user, String fone, String mail) {
+		WebElement nome = driver.findElement(By.name("desperson"));
+		WebElement login = driver.findElement(By.name("deslogin"));
+		WebElement phone = driver.findElement(By.name("nrphone"));
+		WebElement email = driver.findElement(By.name("desemail"));
+		WebElement admin = driver.findElement(By.name("inadmin"));
+		
+		nome.clear();
+		login.clear();
+		phone.clear();
+		email.clear();
+		
+		nome.sendKeys(name);
+		login.sendKeys(user);
+		phone.sendKeys(fone);
+		email.sendKeys(mail);
+		admin.click();
+	}
+	
+	public void submeterForm() {
+		WebElement submit = driver.findElement(By.className("btn-primary"));
+		
+		submit.click();
+	}
+	
 	public boolean existeNaPagina(String search) {
 		return driver.getPageSource().contains(search);
 	}
--- test/test/SystemTest.java
+++ test/test/SystemTest.java
@@ -3,8 +3,10 @@ package test;
 	import static org.junit.Assert.*;
 
 	import org.junit.After;
-	import org.junit.Before;
-	import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 	import org.openqa.selenium.By;
 	import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.WebElement;
@@ -20,7 +22,7 @@ public class SystemTest {
 	private LoginPage login;
 	private RegisterPage register;
 	private UsersPage users;
-
+	
 	@Before
 	public void inicializa() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hiiii\\Downloads\\driver\\chromedriver.exe");
@@ -51,33 +53,17 @@ public class SystemTest {
 	
 	@Test
 	public void deveAlterarUsuarioCadastrado() {
-		driver.get("http://www.ecommerce.com/admin/users");
+		users.acessa();
+		users.alterar();
+		users.preencherForm(
+				"Caio Julio Cesar Otaviano Augusto",
+				"Octavianus",
+				"998877663",
+				"princepsImperator@roma.com"
+				);
+		users.submeterForm();
 		
-		WebElement alter = driver.findElement(By.xpath("/html/body/div/div[1]/section[2]/div/div/div/div[2]/table/tbody/tr[2]/td[6]/a[1]"));
-		alter.click();
-		
-		WebElement nome = driver.findElement(By.name("desperson"));
-		WebElement login = driver.findElement(By.name("deslogin"));
-		WebElement phone = driver.findElement(By.name("nrphone"));
-		WebElement email = driver.findElement(By.name("desemail"));
-		WebElement admin = driver.findElement(By.name("inadmin"));
-		
-		nome.clear();
-		login.clear();
-		phone.clear();
-		email.clear();
-		
-		nome.sendKeys("Caio Julio Cesar Otaviano Augusto");
-		login.sendKeys("Octavianus");
-		phone.sendKeys("998877664");
-		email.sendKeys("princepsImperator@roma.com");
-		admin.click();
-		
-		WebElement submit = driver.findElement(By.className("btn-primary"));
-		
-		submit.click();
-		
-		assertTrue(driver.getPageSource().contains("Caio"));
+		assertTrue(users.existeNaPagina("Caio"));
 	}
 		
 	@Test
@@ -88,8 +74,6 @@ public class SystemTest {
 		assertTrue(!users.existeNaPagina("Caio"));
 	}
 	
-		
-
 	@After
 	public void finaliza() {
 		driver.close();
